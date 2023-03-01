55104c32736c664f5dccec39f00321ebdeaba519
luccasmeyer <lucasmeyer.scs@gmail.com>
2023-03-01 00:53:27 -0300

    tudo funfando iniciando o recycleview

---
+++ app/src/main/java/com/example/fitware/ExercicioAdapter.java
@@ -0,0 +1,28 @@
+package com.example.fitware;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ExercicioAdapter extends RecyclerView.Adapter<ExercicioAdapter.MyViewHolder> {
+
+    private
+
+    @NonNull
+    @Override
+    public ExercicioAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExercicioAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+}
